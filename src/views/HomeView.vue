e970b167347bdf1fe20cca7cd3f22deff9084fe0
Jheng318 <123611711+Jheng318@users.noreply.github.com>
2024-12-08 21:50:50 +0800

    added typescirpt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,11 @@
   <div>
     <h1>Hello</h1>
     <p>hello world</p>
+    <img src="../../public/laptop-icon-02.svg" id="hero-img" />
+    <!-- <i class="devicon-html5-plain" id="#html-icon"></i> -->
   </div>
 </template>
+<script setup>
+// how to do https://web3forms.com/blog/contact-forms-portfolio-websites
+// for the contact form send it to : https://web3forms.com/
+</script>
