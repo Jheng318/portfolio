cbe95c48771a9bc630a4b11b72a679e19c5a4771
Jheng318 <jhchan0514@gmail.com>
2024-12-09 00:26:57 +0800

    error view route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
+import ErrorView from "@/views/ErrorView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/:notFound",
+      component: ErrorView,
     },
   ],
 });
