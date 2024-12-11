e9a56e88c1c4c7ccdf4091cd07063b0c66fa8787
Jheng318 <123611711+Jheng318@users.noreply.github.com>
2024-12-11 20:44:19 +0800

    remove and updated some stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Hello</h1>
     <p>hello world</p>
-    <img src="/laptop-icon-02.svg" id="hero-img" />
+    <img src="../assets/laptop-icon-02.svg" id="hero-img" />
     <!-- <i class="devicon-html5-plain" id="#html-icon"></i> -->
   </div>
 </template>
