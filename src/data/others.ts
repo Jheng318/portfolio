b623b6c35faad1f6888570ee733c228643471f2d
Jheng318 <jhchan0514@gmail.com>
2024-12-29 17:17:36 +0800

    fix data

--- src/data/others.ts
+++ src/data/others.ts
@@ -2,23 +2,23 @@ import type { skillType } from "@/types/skillsTypes";
 const others: skillType[] = [
   {
     name: "Flutter & Dart",
-    img: "../src/assets/flutter-original.svg",
+    img: "./src/assets/flutter-original.svg",
   },
   {
     name: "Python",
-    img: "../src/assets/python-original.svg",
+    img: "./src/assets/python-original.svg",
   },
   {
     name: "Selenium (Java)",
-    img: "../src/assets/selenium-original.svg",
+    img: "./src/assets/selenium-original.svg",
   },
   {
     name: "UiPath (RPA)",
-    img: "../src/assets/uipath-3.svg",
+    img: "./src/assets/uipath-3.svg",
   },
   {
     name: "Adobe Xd",
-    img: "../src/assets/xd-original.svg",
+    img: "./src/assets/xd-original.svg",
   },
 ];
 
