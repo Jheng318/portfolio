a0a1ced0519f4fe57ea683d15bb110469762953b
Jheng318 <jhchan0514@gmail.com>
2024-12-30 23:47:09 +0800

    finished contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,12 @@
   <section id="contact">
     <Loader v-if="isLoading" />
     <form @submit.prevent="submitForm">
+      <p v-if="success" class="success">
+        Your message has successfully been delivered
+      </p>
       <h3>Contact Me</h3>
+
+      <span v-if="errors.name" class="error">{{ errors.name }}</span>
       <input
         type="text"
         class="contactInput"
@@ -10,8 +15,10 @@
         name="name"
         v-model="name"
         placeholder="Full Name"
+        @input="validateName(name)"
       />
       <br />
+      <span v-if="errors.email" class="error">{{ errors.email }}</span>
       <input
         id="contactEmail"
         class="contactInput"
@@ -19,17 +26,19 @@
         name="email"
         v-model="email"
         placeholder="Email Address"
+        @input="validateEmail(email)"
       />
 
       <br />
+      <span v-if="errors.message" class="error">{{ errors.message }}</span>
       <textarea
         id="contactMessage"
         class="contactInput"
         name="message"
         v-model="message"
         placeholder="Your Message"
+        @input="validateMessage(message)"
       ></textarea>
-
       <br />
       <button
         type="submit"
@@ -45,59 +54,86 @@
 <script setup lang="ts">
 import { ref, type Ref } from "vue";
 import Loader from "./Loader.vue";
-const WEB3FORMS_ACCESS_KEY = import.meta.env.VITE_WEB3FORMS_ACCESS_KEY;
-const errors: { [key: string]: string } = {
+const WEB3FORMS_ACCESS_KEY = "51eb1f7c-7973-4bf4-8197-7cd31f08f831";
+
+const errors: Ref<{ [key: string]: string }> = ref({
   name: "",
   email: "",
   message: "",
-};
-const isValid: Ref<boolean> = ref(true);
+});
 const name: Ref<string> = ref("");
 const email: Ref<string> = ref("");
 const message: Ref<string> = ref("");
 const isLoading: Ref<boolean> = ref(false);
-const checkName = () => {};
-const checkEmail = () => {
-  const regex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const success: Ref<string> = ref("");
 
-  if (email.value.length === 0) {
-    errors.email = "Email is required";
-    isValid.value = false;
-  } else if (!regex.test(email.value)) {
-    errors.email = "Please enter a proper email address";
-    isValid.value = false;
+const validateName = (name: string) => {
+  const nameRegex = /^[a-zA-Z\s]{3,50}$/;
+  if (name.length === 0) {
+    errors.value.name = "Please enter your name";
+  } else if (!nameRegex.test(name)) {
+    errors.value.name =
+      "Please enter name with at least 3 letters and maximum of 50 letters";
   } else {
-    isValid.value = true;
-    errors.email = "";
+    errors.value.name = "";
   }
 };
-const checkMessage = () => {};
+const validateEmail = (email: string) => {
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (email.length === 0) {
+    errors.value.email = "Please enter your email";
+  } else if (!emailRegex.test(email)) {
+    errors.value.email = "Please enter a valid email address";
+  } else {
+    errors.value.email = "";
+  }
+};
+const validateMessage = (message: string) => {
+  if (message.length === 0) {
+    errors.value.message = "Please enter your message";
+  } else if (message.length < 10) {
+    errors.value.message = "Message should be at least 10 characters long";
+  } else {
+    errors.value.message = "";
+  }
+};
+
 const submitForm = async () => {
-  isLoading.value = true;
+  validateName(name.value);
+  validateEmail(email.value);
+  validateMessage(message.value);
+  if (!errors.value.name && !errors.value.email && !errors.value.message) {
+    isLoading.value = true;
 
-  const response = await fetch("https://api.web3forms.com/submit", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      access_key: WEB3FORMS_ACCESS_KEY,
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    }),
-  });
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        access_key: WEB3FORMS_ACCESS_KEY,
+        name: name.value,
+        email: email.value,
+        message: message.value,
+      }),
+    });
 
-  const result = await response.json();
+    const result = await response.json();
 
-  isLoading.value = false;
-  name.value = "";
-  email.value = "";
-  message.value = "";
-  if (result.success) {
-    console.log(result);
+    isLoading.value = false;
+    name.value = "";
+    email.value = "";
+    message.value = "";
+    if (result.success) {
+      success.value = result.success;
+      setTimeout(() => {
+        success.value = "";
+      }, 5000);
+    }
+  } else {
+    return;
   }
 };
 </script>
--- src/data/back-end.ts
+++ src/data/back-end.ts
@@ -2,23 +2,23 @@ import type { skillType } from "@/types/skillsTypes";
 const backEndSkills: skillType[] = [
   {
     name: "C# ASP.NET MVC",
-    img: "./src/assets/csharp-original.svg",
+    img: "@/assets/csharp-original.svg",
   },
   {
     name: "PHP & phpMyAdmin",
-    img: "./src/assets/php-original.svg",
+    img: "@/assets/php-original.svg",
   },
   {
     name: "Laravel (API)",
-    img: "./src/assets/laravel-original.svg",
+    img: "@/assets/laravel-original.svg",
   },
   {
     name: "Node.js (API)",
-    img: "./src/assets/nodejs-original.svg",
+    img: "@/assets/nodejs-original.svg",
   },
   {
     name: "MSSQL",
-    img: "./src/assets/sqldeveloper-original.svg",
+    img: "@/assets/sqldeveloper-original.svg",
   },
 ];
 export default backEndSkills;
